465e41116640cfb697458f4b487c5f45639efb85
Tristanvivaldy <2172032@maranatha.ac.id>
2025-03-24 19:22:11 +0700

    section resume update

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -5,21 +5,27 @@
     >
       RESUME
     </div>
-    <div class="grid grid-cols-12 mt-15">
-      <div class="section-education col-span-6">
-        <div class="text-2xl">
-          <div class="sub-title crimson-text-semibold">Education</div>
+    <div class="grid grid-cols-12 mt-15 gap-10 justify-center items-center">
+      <div class="col-span-6">
+        <div class="section-education">
+          <div class="sub-title crimson-text-semibold text-2xl">Education</div>
           <div
-            class="content-1-education w-10/12 h-24 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4"
+            class="content-1-education w-full h-28 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4 grid grid-cols-12"
           >
-            <div class="grid grid-cols-12 justify-center items-center">
-              <div class="col-span-9 text-2xl crimson-text-bold">
-                SMAN 2 Kota Bekasi
-              </div>
-              <div class="col-span-3 text-lg text-end">2017-2020</div>
+            <div class="col-span-2 flex justify-center items-center w-20 pr-3">
+              <img src="../assets/logo-sman2.png" alt="logo-etniz" />
             </div>
-            <div class="text-lg text-pink-600 crimson-text-semibold">
-              Math & Science Major
+            <div class="col-span-10">
+              <div class="grid grid-cols-12 justify-center items-center">
+                <div class="col-span-9 text-xl crimson-text-bold">
+                  SMAN 2 Kota Bekasi
+                </div>
+                <div class="col-span-3 text-lg text-end">2017-2020</div>
+              </div>
+              <div class="text-lg text-pink-600 crimson-text-semibold">
+                Math & Science Major
+              </div>
+              <div class="text-sm mt-1">Final Grade : 84.6</div>
             </div>
           </div>
           <div class="tree center-line absolute left-0 -mt-8">
@@ -36,12 +42,201 @@
             </div>
           </div>
           <div
-            class="content-1-education w-1/2 mt-5 mb-10 h-24 bg-white rounded-lg"
-          ></div>
+            class="content-1-education w-full h-28 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4 grid grid-cols-12"
+          >
+            <div class="col-span-2 flex justify-center items-center w-20 pr-3">
+              <img src="../assets/logo-maranatha.png" alt="logo-marnat" />
+            </div>
+            <div class="col-span-10">
+              <div class="grid grid-cols-12 justify-center items-center">
+                <div class="col-span-9 text-xl crimson-text-bold">
+                  Maranatha Christian University
+                </div>
+                <div class="col-span-3 text-lg text-end">2021-2025</div>
+              </div>
+              <div class="text-lg text-pink-600 crimson-text-semibold">
+                Bachelor of Computer Science
+              </div>
+              <div class="text-sm mt-1">GPA : 3.93</div>
+            </div>
+          </div>
         </div>
-        <div>Organization</div>
+        <div class="section-organization">
+          <div class="sub-title crimson-text-semibold text-2xl">
+            Organization & Volunteer
+          </div>
+          <div
+            class="content-1-education w-full h-94 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4"
+          >
+            <div class="grid grid-cols-12 justify-center items-center">
+              <div class="col-span-2 flex justify-center items-center w-20">
+                <img src="../assets/logo-semafit.png" alt="logo-semafit" />
+              </div>
+              <div class="col-span-10 text-xl crimson-text-bold">
+                <p>SEMAFIT</p>
+                <div class="text-sm crimson-text-regular">
+                  Senat Mahasiswa Fakultas Teknologi Informasi Universitas
+                  Kristen Maranatha
+                </div>
+              </div>
+            </div>
+            <hr class="my-2 border-1" />
+            <div class="grid grid-cols-12 text-lg text-pink-600">
+              <div class="col-span-8 crimson-text-semibold">
+                Public Relations Coordinator
+              </div>
+              <div class="col-span-4 text-end">Jan 2022 - Jan 2023</div>
+            </div>
+            <div class="text-sm mt-2">
+              <li>
+                Organize the public relations division to work in accordance
+                with their capabilities.
+              </li>
+              <li>
+                Creating 2+ work programs such as valentine, charity, and also
+                comparative studies with other universities.
+              </li>
+              <li>Being a mc in 3+ committee events.</li>
+              <li>
+                Responsible for the performance of members and public relations
+                work programs.
+              </li>
+            </div>
+            <div class="grid grid-cols-12 text-lg text-pink-600 mt-6">
+              <div class="col-span-8 crimson-text-semibold">
+                Vice Public Relations Coordinator
+              </div>
+              <div class="col-span-4 text-end">Jan 2023 - Jan 2024</div>
+            </div>
+            <div class="text-sm mt-2">
+              <li>
+                Provide advice and experience to the public relations division.
+              </li>
+              <li>Responsible being a committee of 5+ work program.</li>
+            </div>
+          </div>
+          <div class="tree center-line absolute left-0 mt-30">
+            <div class="bg-white border-1 w-16"></div>
+            <div class="relative left-16 -mt-95 bg-white border-1 w-0 h-160">
+              <div
+                class="point absolute -left-2 -top-1 bg-white w-4 h-4 border-1 rounded-full"
+              ></div>
+              <div
+                class="point absolute -left-2 top-92.5 bg-white w-4 h-4 border-1 rounded-full"
+              ></div>
+              <div
+                class="point absolute -left-2 -bottom-1 bg-white w-4 h-4 border-1 rounded-full"
+              ></div>
+              <div class="line absolute bg-white top-1 border-1 w-9"></div>
+              <div class="line absolute bg-white top-94.5 border-1 w-9"></div>
+              <div class="line absolute bg-white bottom-1 border-1 w-9"></div>
+            </div>
+          </div>
+          <div
+            class="content-1-education w-full h-56 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4"
+          >
+            <div class="grid grid-cols-12 justify-center items-center">
+              <div class="col-span-2 flex justify-center items-center w-20">
+                <img src="../assets/logo-gdg.png" alt="logo-gdg" />
+              </div>
+              <div class="col-span-10 text-xl crimson-text-bold">
+                <p>Google Developer Groups Bandung (GDG)</p>
+              </div>
+            </div>
+            <hr class="mb-3 border-1" />
+            <div class="grid grid-cols-12 text-lg text-pink-600">
+              <div class="col-span-8 crimson-text-semibold">
+                Runner & Publication
+              </div>
+              <div class="col-span-4 text-end">Sep 2023 - Dec 2023</div>
+            </div>
+            <div class="text-sm mt-2">
+              <li>Creating 10+ captions for Instagram posts.</li>
+              <li>Setting up the venue for 1000+ guests during the events.</li>
+              <li>
+                Assist in processing an average of 100+ walk-in attendees at the
+                registration desk.
+              </li>
+            </div>
+          </div>
+          <div
+            class="content-1-education w-full h-44 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4"
+          >
+            <div class="grid grid-cols-12 justify-center items-center">
+              <div class="col-span-2 flex justify-center items-center w-20">
+                <img src="../assets/logo-ieee.png" alt="logo-ieee" />
+              </div>
+              <div class="col-span-10 text-xl crimson-text-bold">
+                <p>IEEE International Conference</p>
+              </div>
+            </div>
+            <hr class="my-3 border-1" />
+            <div class="grid grid-cols-12 text-lg text-pink-600">
+              <div class="col-span-8 crimson-text-semibold">Usher</div>
+              <div class="col-span-4 text-end">Jan 2023 - Jun 2023</div>
+            </div>
+            <div class="text-sm mt-2">
+              <li>Setting up EDAS Website.</li>
+              <li>Assist 20+ invitation guests to their seats at the event.</li>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="section-education col-span-6">Experience</div>
+      <div class="section-education col-span-6">
+        <div class="sub-title crimson-text-semibold text-2xl text-end">
+          Experience
+        </div>
+        <div
+          class="content-1-education w-full h-94 mt-5 mb-15 bg-white rounded-lg text-black px-5 py-4"
+        >
+          <div class="grid grid-cols-12 justify-center items-center">
+            <div class="col-span-2 flex justify-center items-center w-20">
+              <img src="../assets/logo-semafit.png" alt="logo-semafit" />
+            </div>
+            <div class="col-span-10 text-xl crimson-text-bold">
+              <p>SEMAFIT</p>
+              <div class="text-sm crimson-text-regular">
+                Senat Mahasiswa Fakultas Teknologi Informasi Universitas Kristen
+                Maranatha
+              </div>
+            </div>
+          </div>
+          <hr class="my-2 border-1" />
+          <div class="grid grid-cols-12 text-lg text-pink-600">
+            <div class="col-span-8 crimson-text-semibold">
+              Public Relations Coordinator
+            </div>
+            <div class="col-span-4 text-end">Jan 2022 - Jan 2023</div>
+          </div>
+          <div class="text-sm mt-2">
+            <li>
+              Organize the public relations division to work in accordance with
+              their capabilities.
+            </li>
+            <li>
+              Creating 2+ work programs such as valentine, charity, and also
+              comparative studies with other universities.
+            </li>
+            <li>Being a mc in 3+ committee events.</li>
+            <li>
+              Responsible for the performance of members and public relations
+              work programs.
+            </li>
+          </div>
+          <div class="grid grid-cols-12 text-lg text-pink-600 mt-6">
+            <div class="col-span-8 crimson-text-semibold">
+              Vice Public Relations Coordinator
+            </div>
+            <div class="col-span-4 text-end">Jan 2023 - Jan 2024</div>
+          </div>
+          <div class="text-sm mt-2">
+            <li>
+              Provide advice and experience to the public relations division.
+            </li>
+            <li>Responsible being a committee of 5+ work program.</li>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
